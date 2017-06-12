367f5643f0a1993a1f1885790de8bad8dc5151c7
@genevieveagar Agar <genevieveagar@gmail.com>
2017-06-12 00:44:42 +0000

    Commit all changes

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
-// declare your functions here...
+// first paragraphSelector
+$(paragraphSelector( $ 'p'){
+      return 'p';
+   });
+
+  //last image selector
+  $(lastImageSelector($ 'img:last'){
+    return 'img:last'
+  });
+
+  //baby Ninja!!!
+
+  $(ninjaBabySelector( $ '#baby-ninja'){
+    return '#baby-ninja.img'
+  });
+
+  //div Selector
+  $(divSelector( $ 'div.'){
+    return 'div.pics'
+  });
+
+  //first list item!
+  $(firstListItem ($ 'div.'){
+    return 'ul#pic-list:first-child'
+  });
